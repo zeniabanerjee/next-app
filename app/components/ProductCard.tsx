a7c0e11c84045accf7f981d1298e026342c8b2a0
Zenia Banerjee <zenia.banerjee@gmail.com>
2024-08-08 01:17:49 +0530

    feat: basics of next added

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+
+const ProductCard = () => {
+  return (
+    <div className="">
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
